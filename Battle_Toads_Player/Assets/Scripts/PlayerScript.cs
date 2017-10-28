625b9c5d5227355ea32c801a100da825d04f7a95
draskin2 <draskin2@illinois.edu>
2017-10-28 11:46:54 -0500

    Made player, jumps up and down

---
+++ Battle_Toads_Player/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float jumpPower = 10.0f;
+    Rigidbody2D playerRigidbody;
+    bool isGrounded = false;
+
+	// Use this for initialization
+	void Start () {
+        playerRigidbody = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.Space) && isGrounded) {
+            playerRigidbody.AddForce(Vector3.up * (jumpPower * playerRigidbody.mass * playerRigidbody.gravityScale * 20.0f));
+        }
+	}
+
+    void OnCollisionEnter2D (Collision2D other) {
+        if (other.collider.tag == "Ground") {
+            Debug.Log("Yayayyayay");
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other) {
+        if (other.collider.tag == "Ground") {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
