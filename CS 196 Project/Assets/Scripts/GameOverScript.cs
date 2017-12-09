231c7ad68f097b9e599c213452ae15ae45f4b24d
ThePranav <jpkonuru@gmail.com>
2017-12-09 12:03:44 -0600

    Added scripts necessary for game over panel

---
+++ CS 196 Project/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScript : MonoBehaviour {
+
+    //A UI Panel GameObject
+    public GameObject gameOverPanel;
+    //A UI Text element
+    public Text gameOverText;
+    
+    //Sets the Panel inactive at the start of the game. Should be called at the start of every game
+    public void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
+
+    public void Update()
+    {
+        GameOver();
+    }
+    //Sets the Panel active. Should be called upon player death
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+        gameOverText.text = "Game Over!";
+    }
+}
