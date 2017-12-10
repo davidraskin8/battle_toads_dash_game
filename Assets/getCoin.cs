6f26497356888f4e841b0da905d44e0d5338377a
Alice Fang <alicesf2@illinois.edu>
2017-12-09 20:53:56 -0600

    plz work now

---
+++ Assets/getCoin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//collecting coins
+public class getCoin : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //increment score by 1
+        PlayerClass.playerScore++;
+        //destroy coin
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/triangleobstacle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//GameControl refers to class that contains player controls and behavior
+public class triangleobstacle : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        //If player hits obstacle, game over
+        if (collision.GetComponent<player>() = null)
+            GameControl.instance.PlayerDied();
+    }
